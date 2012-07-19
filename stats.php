52a8a1eedd34dd781281948f838c29789c2b8062
Nathan Lex <ilikesmashingstuff@gmail.com>
2012-07-19 00:59:04 -0500

    Some basic work done on stats.php. It accepts filtering by room and activity and displays resulting ids.

--- stats.php
+++ stats.php
@@ -4,5 +4,22 @@
     $connection = pg_connect ("host=$dbhost dbname=$dbname 
                               user=$dbuser password=$dbpass");
 
-    $query = "SELECT id FROM data WHERE ;";
+    $filterstr = "";
+    if (array_key_exists("activity", $_GET) or array_key_exists("room", $_GET)) {
+      $filterstr .= " WHERE";
+      $filterstr .= array_key_exists("activity", $_GET) ? " activity = {$_GET["activity"]}" : "";
+      $filterstr .= (array_key_exists("activity", $_GET) and array_key_exists("room", $_GET)) ? "," : "";
+      $filterstr .= array_key_exists("room", $_GET) ? " room = {$_GET["room"]}" : "";
+    }
+    $query = "SELECT id, visitor FROM data$filterstr;";
+
+    $result = pg_query($connection, $query) or 
+      die("Error in query: $query." . pg_last_error($connection));
+    
+    echo $query . "<br/>\n";
+    
+    while ($row = pg_fetch_assoc($result)) {
+         print_r($row);
+         echo "<br/>\n";
+    } 
 ?> 

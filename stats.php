dcd167649f49958c35a3125500c8340b33edbe1a
Nathan Lex <ilikesmashingstuff@gmail.com>
2012-07-19 02:44:55 -0500

    Stats.php now has a html form.

--- stats.php
+++ stats.php
@@ -8,14 +8,57 @@
 ?>
 
 <div class="span9 well" style="overflow-x: auto;">
+  <form class="form-horizontal" action="" method="get">
+    <fieldset>
+      <div class="control-group">
+        <label class="control-label" for="activity">Activity</label>
+        <div class="controls">
+          <select name="activity" id="activity">
+            <option value="any" selected> -- Any -- </option>
+            <?php
+              $query = "SELECT id, name FROM activities;";
+              $result = pg_query($connection, $query) or
+                die("Error in query: $query." . pg_last_error($connection));
+              while ($data = pg_fetch_assoc($result)) {
+                echo "<option value=\"{$data["id"]}\">{$data["name"]}</option>\n";
+              }
+              pg_free_result($result);
+            ?>
+          </select>
+        </div>
+      </div>
+      <div class="control-group">
+        <label class="control-label" for="room">Room</label>
+        <div class="controls">
+          <select name="room" id="room">
+            <option value="any" selected> -- Any -- </option>
+            <?php
+              $query = "SELECT id, name FROM rooms;";
+              $result = pg_query($connection, $query) or
+                die("Error in query: $query." . pg_last_error($connection));
+              while ($data = pg_fetch_assoc($result)) {
+                echo "<option value=\"{$data["id"]}\">{$data["name"]}</option>\n";
+              }
+              pg_free_result($result);
+            ?>
+          </select>
+        </div>
+      </div>
+      <div class="form-actions">
+        <input type="submit" class="btn btn-primary" value="Filter" />
+      </div>
+    </fieldset>
+  </form>
 <?php
 
+    $fba = (is_numeric($_GET["activity"]) and $_GET["activity"] >= 1);
+    $fbr = (is_numeric($_GET["room"]) and ($_GET["room"]) >= 1);
     $filterstr = "";
-    if (array_key_exists("activity", $_GET) or array_key_exists("room", $_GET)) {
+    if ($fba or $fbr) {
       $filterstr .= " WHERE";
-      $filterstr .= array_key_exists("activity", $_GET) ? " activity = {$_GET["activity"]}" : "";
-      $filterstr .= (array_key_exists("activity", $_GET) and array_key_exists("room", $_GET)) ? "," : "";
-      $filterstr .= array_key_exists("room", $_GET) ? " room = {$_GET["room"]}" : "";
+      $filterstr .= $fba ? " activity = {$_GET["activity"]}" : "";
+      $filterstr .= ($fba and $fbr) ? " and" : "";
+      $filterstr .= $fbr ? " room = {$_GET["room"]}" : "";
     }
     $query = "SELECT id, visitor FROM data$filterstr;";
 
@@ -31,7 +74,6 @@
     
     pg_free_result($result);
 ?> 
-
 </div>
 <?php
   require_once "template/footer.php" ;

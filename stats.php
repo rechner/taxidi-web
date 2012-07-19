d9616c93fbbc392deed3f52f96f6f936a7aaf192
Nathan Lex <ilikesmashingstuff@gmail.com>
2012-07-19 16:31:29 -0500

    More stats.php work, including the ability to filter by date.

--- stats.php
+++ stats.php
@@ -11,6 +11,28 @@
   <form class="form-horizontal" action="" method="get">
     <fieldset>
       <div class="control-group">
+        <label class="control-label" for="activity">Services</label>
+        <div class="controls">
+          <select name="service" id="service">
+            <option value="any" selected> -- Any -- </option>
+            <?php
+              $query = "SELECT id, name FROM services;";
+              $result = pg_query($connection, $query) or
+                die("Error in query: $query." . pg_last_error($connection));
+              while ($data = pg_fetch_assoc($result)) {
+                echo "<option value=\"{$data["id"]}\"";
+                if ( $data["id"] == $_GET["service"] ) {
+                  echo " selected";
+                  $service = $data["name"];
+                }
+                echo ">{$data["name"]}</option>\n";
+              }
+              pg_free_result($result);
+            ?>
+          </select>
+        </div>
+      </div>
+      <div class="control-group">
         <label class="control-label" for="activity">Activity</label>
         <div class="controls">
           <select name="activity" id="activity">
@@ -20,7 +42,12 @@
               $result = pg_query($connection, $query) or
                 die("Error in query: $query." . pg_last_error($connection));
               while ($data = pg_fetch_assoc($result)) {
-                echo "<option value=\"{$data["id"]}\"" . ($data["id"] == $_GET["activity"] ? " selected" : "") . ">{$data["name"]}</option>\n";
+                echo "<option value=\"{$data["id"]}\"";
+                if ( $data["id"] == $_GET["activity"] ) {
+                  echo" selected";
+                  $activity = $data["name"];
+                }
+                echo ">{$data["name"]}</option>\n";
               }
               pg_free_result($result);
             ?>
@@ -37,11 +64,30 @@
               $result = pg_query($connection, $query) or
                 die("Error in query: $query." . pg_last_error($connection));
               while ($data = pg_fetch_assoc($result)) {
-                echo "<option value=\"{$data["id"]}\"" . ($data["id"] == $_GET["room"] ? " selected" : "") . ">{$data["name"]}</option>\n";
+                echo "<option value=\"{$data["id"]}\"";
+                if ( $data["id"] == $_GET["room"] ) {
+                  echo" selected";
+                  $room = $data["name"];
+                }
+                echo ">{$data["name"]}</option>\n";
               }
               pg_free_result($result);
             ?>
           </select>
+        </div>
+      </div>
+      <div class="control-group form-inline">
+        <label class="control-label" for="date">Date</label>
+        <div class="controls">
+	        <label class="radio inline">
+	          <input type="radio" style="margin-bottom: 9px;" name="datef" value="any" checked>
+	        </label>
+          Any date
+          <br/>
+	        <label class="radio inline">
+	          <input type="radio" name="datef" value="single"<?php echo ($_GET["datef"] == "single" ? "checked" : ""); ?>>
+	        </label>
+          <input type="text" class="input-small" style="height: 28px;" name="date" id="date" value="<?php echo date("Y-m-d"); ?>">
         </div>
       </div>
       <div class="form-actions">
@@ -51,17 +97,22 @@
   </form>
 <?php
 
-    $fba = (is_numeric($_GET["activity"]) and $_GET["activity"] >= 1);
-    $fbr = (is_numeric($_GET["room"]) and ($_GET["room"]) >= 1);
-    $filterstr = "";
-    if ($fba or $fbr) {
-      $filterstr .= " WHERE";
-      $filterstr .= $fba ? " activity = {$_GET["activity"]}" : "";
-      $filterstr .= ($fba and $fbr) ? " and" : "";
-      $filterstr .= $fbr ? " room = {$_GET["room"]}" : "";
+    $filters = array();
+    if (is_numeric($_GET["service"]) and $_GET["service"] >= 1) {
+      $filters[] = "service LIKE '$service'";
     }
-    $query = "SELECT id, visitor FROM data$filterstr;";
-
+    if (is_numeric($_GET["activity"]) and $_GET["activity"] >= 1) {
+      $filters[] = "activity LIKE '$activity'";
+    }
+    if (is_numeric($_GET["room"]) and $_GET["room"] >= 1) {
+      $filters[] = "room LIKE '$room'";
+    }
+    if ($_GET["datef"] == "single") {
+      $filters[] = "date = DATE '{$_GET["date"]}'";
+    }
+    
+    $query = "SELECT person, volunteer FROM statistics" . (count($filters) > 0 ? " WHERE " . implode(" and ", $filters) : "") . ";";
+    
     $result = pg_query($connection, $query) or 
       die("Error in query: $query." . pg_last_error($connection));
     

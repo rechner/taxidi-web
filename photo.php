8c2a18046b18a9f5d5ec7c2afa4c7dc574a8ea70
Nathan Lex <ilikesmashingstuff@gmail.com>
2012-08-06 19:42:07 -0500

    Long over due commit. Photo upload works, but it's not pretty yet.

--- photo.php
+++ photo.php
@@ -2,21 +2,23 @@
 	require_once 'config.php';
 	
 	if (is_numeric($_GET["id"])) {
-	header("Content-Type: image/jpeg");
-		$file = $photo_path.str_pad($_GET["id"], 6, "0", 0).".jpg";
+    //TODO: check if file exists.
+		$files = glob($photo_path . str_pad($_GET["id"], 6, "0", 0) . "*");
+		$file = $files[0];
 	} else {
-		header("Content-Type: image/png");
 		$file = "resources/nophoto.png";
 	}
   
 	if(array_key_exists("HTTP_IF_MODIFIED_SINCE",$_SERVER)) {
-    $ims = strtotime(preg_replace('/;.*$/','',$_SERVER["HTTP_IF_MODIFIED_SINCE"]));
-    if($ims >= filemtime($file)) {
-        header("HTTP/1.0 304 Not Modified");
-        exit();
+		$ims = strtotime(preg_replace('/;.*$/','',$_SERVER["HTTP_IF_MODIFIED_SINCE"]));
+		if($ims >= filemtime($file)) {
+      header("HTTP/1.0 304 Not Modified");
+      exit();
     }
-  }
+ 	}
   
+	//TODO: check for valid image mime type
+	header("Content-Type: " . mime_content_type($file));
   header("Last-Modified: " . date("r", filemtime($file)));
   readfile($file);
   

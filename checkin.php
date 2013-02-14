35c082dd149e5cd4aec62b153c1c8e497c17802e
Nathan Lex <ilikesmashingstuff@gmail.com>
2013-02-13 19:21:59 -0600

    Simplified checkin.php

--- checkin.php
+++ checkin.php
@@ -2,13 +2,6 @@
   require_once 'config.php';
   require_once 'functions.php';
   
-  function get_code() {
-    ob_start();
-    include "/usr/lib/cgi-bin/secure-code.php";
-    ob_end_clean();
-    return $read[$i];
-  }
-  
   $sql = "INSERT INTO statistics(person, date, service, expires, checkin, code, location, activity, room)
             SELECT :person, current_date, (SELECT name FROM services WHERE id = :service),
               '23:59:59', now()::timestamp, :code, :location, activities.name, rooms.name FROM data 
@@ -17,10 +10,12 @@
               WHERE data.id = :person;";
   $dbh = db_connect();
   $sth = $dbh->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+  ob_start();
+  include "/usr/lib/cgi-bin/secure-code.php";
   $sth->execute(array(
     ":person"   => $_REQUEST["person"],
     ":service"  => $_REQUEST["service"],
-    ":code"     => get_code(),
+    ":code"     => ob_get_clean(),
     ":location" => get_client_ip()
   ));
   

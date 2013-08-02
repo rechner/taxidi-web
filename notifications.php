777240da9155e7695ac1aaab211de39de98aa031
Nathan Lex <ilikesmashingstuff@gmail.com>
2013-08-02 14:26:44 -0500

    Added basic pdo polling code.

--- notifications.php
+++ notifications.php
@@ -1,19 +1,42 @@
 <?php
   require_once 'config.php';
   require_once 'functions.php';
-  $dbh = db_connect();
   
   session_assert_valid();
+  $dbh = db_connect();
   
-  if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  $method = $_SERVER['REQUEST_METHOD'];
+  if ($method == 'POST') {
     $sql = 'INSERT INTO notifications (
       message, priority, location) VALUES (
       :message, :priority, :location);';
-    $sth = $dbh->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+    $sth = $dbh->prepare($sql);
     $sth->execute(array(
       ':message'  => $_REQUEST['message'],
       ':priority' => $_REQUEST['priority'],
       ':location' => $_REQUEST['location']
     ));
+  } elseif ($method == 'GET') {
+    ignore_user_abort(false);
+    $sql = 'SELECT MAX(datetime) FROM notifications';
+    $sth = $dbh->prepare($sql);
+    $sth->execute();
+    $old = $sth->fetchColumn();
+    $sth->closeCursor(); 
+    $last = $old;
+    while ($last == $old) {
+      usleep(1000);
+      $sth->execute();
+      $last = $sth->fetchColumn(); 
+      $sth->closeCursor();
+    }
+    $sth = $dbh->prepare('SELECT * FROM notifications
+      WHERE isunread = true AND datetime > :olddate
+      ORDER BY datetime');
+    $sth->execute(array(':olddate' => $old));
+    while ($row = $sth->fetch(PDO::FETCH_ASSOC)) {
+      print_r($row);
+    }
+    $sth->closeCursor();
   }
 ?>
